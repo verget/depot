521de0d6d3c22561cae1ce7ebf7b2f8cc9b9789d
verget <verget@qip.ru>
2014-07-18 15:44:37 +0400

    Combine items in cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+  
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+  
   def index
+    increment_counter
     @products = Product.order(:title)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,21 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   def self.latest
     Product.order(:updated_at).last
   end
+  
+  private
+    
+   # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add() #:base, 'существуют товарные позиции'
+    return false
+    end
+  end
+  
 end
